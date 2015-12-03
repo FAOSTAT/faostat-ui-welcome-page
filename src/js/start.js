1b9555f0bc2191fb2ca08e79b046ab234ce8431b
Kalimaha <guido.barbaglia@gmail.com>
2015-12-03 13:27:24 +0100

    FileTitle

--- src/js/start.js
+++ src/js/start.js
@@ -1,11 +1,13 @@
 /*global define*/
 define(['jquery',
+        'config/Config',
         'handlebars',
         'text!faostat_ui_welcome_page/html/templates.hbs',
         'i18n!faostat_ui_welcome_page/nls/translate',
+        'faostatapiclient',
         'bootstrap',
         'sweetAlert',
-        'amplify'], function ($, Handlebars, templates, translate) {
+        'amplify'], function ($, C, Handlebars, templates, translate, FAOSTATAPIClient) {
 
     'use strict';
 
@@ -18,7 +20,9 @@ define(['jquery',
             placeholder_id: 'faostat_ui_welcome_page',
             url_wds_crud: 'http://fenixapps2.fao.org/wds_5.1/rest/crud',
             isRendered: false,
-            domain_name: undefined
+            domain_name: undefined,
+            domain_code: undefined,
+            base_url: 'http://faostat.fao.org/Portals/_Faostat/Downloads/FAOSTATFiles/'
 
         };
 
@@ -32,6 +36,9 @@ define(['jquery',
         /* Fix the language, if needed. */
         this.CONFIG.lang = this.CONFIG.lang !== null ? this.CONFIG.lang : 'en';
 
+        /* Initiate FAOSTAT API's client. */
+        this.CONFIG.api = new FAOSTATAPIClient();
+
         /* Render, if needed. */
         if (this.CONFIG.isRendered === false) {
             this.render();
@@ -42,10 +49,36 @@ define(['jquery',
     WELCOME_PAGE.prototype.render = function () {
 
         /* Variables. */
+        var that = this,
+            documents = [],
+            i;
+
+        /* Query DB for available files. */
+        this.CONFIG.api.documents({
+            datasource: C.DATASOURCE,
+            domain_code: this.CONFIG.domain_code
+        }).then(function (data) {
+            for (i = 0; i < data.data.length; i += 1) {
+                documents.push({
+                    FileName: data.data[i].FileName,
+                    FileTitle: data.data[i].FileTitle,
+                    FileContent: data.data[i].FileContent,
+                    base_url: that.CONFIG.base_url
+                });
+            }
+            that.load_template(documents);
+        });
+
+    };
+
+    WELCOME_PAGE.prototype.load_template = function (documents) {
+
+        /* Variables. */
         var source,
             template,
             dynamic_data,
-            html;
+            html,
+            hasDocuments = documents.length > 0;
 
         /* Load main structure. */
         source = $(templates).filter('#faostat_ui_welcome_page_structure').html();
@@ -60,7 +93,9 @@ define(['jquery',
             metadata_title: translate.metadata_title,
             metadata_text: translate.metadata_text,
             related_documents: translate.related_documents,
-            domain_name: this.CONFIG.domain_name
+            domain_name: this.CONFIG.domain_name,
+            hasDocuments: hasDocuments,
+            documents: documents
         };
         html = template(dynamic_data);
         $('#' + this.CONFIG.placeholder_id).html(html);
